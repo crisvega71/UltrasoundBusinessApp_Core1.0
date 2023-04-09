4371c831dff4d417c5a59aac377abb646721a8e4
Cris Vega <crisvega71@gmail.com>
2023-04-09 14:09:42 +0800

    Add project files.

---
+++ UltrasoundBusinessApp_Core1.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UltrasoundBusinessApp_Core1._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
